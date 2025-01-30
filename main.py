500d6cbd0d1706bf1bc5ba3d67a363aba7d238e0
aisuluu777 <a30139706@gmail.com>
2025-01-30 17:53:36 +0600

    completed exam

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import asyncio
 
 from aiogram import Bot
-
 from bot_config import bot, dp, database
-
 from handlers.start import start_router
 from handlers.dialoge import hw_router
+import logging
 
 
 async def on_startup(bot: Bot):
@@ -18,3 +17,7 @@ async def main():
     dp.include_router(start_router)
     dp.include_router(hw_router)
     await dp.start_polling()
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
